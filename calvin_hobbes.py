a9b03ffbd91e90001b63015e6de732c1c44a6619
Shubham Paramahns <shubhamparamhans@gmail.com>
2015-05-07 23:11:59 +0530

    Little better ux :D

--- calvin_hobbes.py
+++ calvin_hobbes.py
@@ -4,7 +4,7 @@ import urllib
 import os
 from threading import Thread
 
-def find_and_download(ids, folder_name, no):
+def find_and_download(ids, folder_name, day, month, year):
 	req_url = "http://www.gocomics.com/api/feature/"+ids+"/item.xml?client_code=KAJS6R5FJAS3"
 	
 	req = urllib2.Request(url=req_url, headers = {'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"', 'User-agent':'Mozilla/5.0'})
@@ -24,15 +24,18 @@ def find_and_download(ids, folder_name, no):
 	data_root = data_tree.getroot()
 	data_objects = data_root.findall('image-link')
 
-	name = no+"_"+id_is+'.png'
-	print name
+	name = day+"-"+month+"-"+year+'.png'
+
+	print("********************Download initiated for "+day+"-"+month+"-"+year+"********************\n")
 
 	urllib.urlretrieve(data_objects[0].text, folder_name+name+".gif")
+
+	print("********************Download completed for "+day+"-"+month+"-"+year+"********************\n")
 	os.remove(id_file)
 
 
-year_data = raw_input("Enter year: ")
-month_data = raw_input("Enter month: ")
+year_data = raw_input("Enter year(yyyy): ")
+month_data = raw_input("Enter month(mm): ")
 directory_path = year_data+'/'+month_data+'/';
 
 if not os.path.exists(directory_path):
@@ -52,12 +55,13 @@ b.close()
 tree = ET.parse('new_calvin.xml')
 root = tree.getroot()
 objects = root.findall('object')
-index = 0
+index = 1
 for obj in objects:
 	id_is = obj.findall('id')[0].text
 	try:
-		t= Thread(target=find_and_download, args=(id_is,directory_path, str(index)))
+		t= Thread(target=find_and_download, args=(id_is,directory_path, str(index), month_data, year_data))
 		t.start()
 		index = index+1
 	except:
-		print("unable to start for "+id_is)
+		date = index+"-"+month_data+"-"+year_data
+		print("unable to fetch for "+date) 
